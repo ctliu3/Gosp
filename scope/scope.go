13ab835eb5be95c02dd61d6be2ec2cb86ffe75f8
ctliu3 <lcndn3@gmail.com>
2015-09-03 15:34:24 +0800

    Simple scope implementation.

--- scope/scope.go
+++ scope/scope.go
@@ -1,5 +1,10 @@
 package scope
 
+import (
+  "github.com/ctliu3/gosp/value"
+  "github.com/ctliu3/gosp/procs"
+)
+
 type Scope struct {
   outer *Scope
   objects map[string]*Object
@@ -9,8 +14,8 @@ type ObjectType int
 
 const (
   Bad ObjectType = iota
-  Fun
   Var
+  Proc
 )
 
 type Object struct {
@@ -18,15 +23,47 @@ type Object struct {
   Data interface{}
 }
 
-func NewScope() *Scope {
+func NewObj(data interface{}) *Object {
+  switch data.(type) {
+  case value.Value:
+    return &Object{Var, data}
+  case procs.Proc:
+    return &Object{Proc, data}
+  }
+  return nil
+}
+
+
+func NewEmtpyScope(outer *Scope) *Scope {
   return &Scope{nil, make(map[string]*Object)}
 }
 
-func (self *Scope) Lookup(ident string) bool {
-  if _, ok := self.objects[ident]; ok {
-    return true
+func NewScope(outer *Scope) *Scope {
+  objs := map[string]*Object {
+    "+": NewObj(procs.NewAdd()),
+    "-": NewObj(procs.NewSub()),
   }
-  return false
+
+  return &Scope{nil, objs}
 }
 
-// Insert
+func (self *Scope) Insert(name string, obj *Object) (alt *Object) {
+  if alt = self.objects[name]; alt == nil {
+    self.objects[name] = obj
+  }
+  return nil
+}
+
+func (self *Scope) Lookup(name string, recur bool) *Object {
+  if !recur {
+    return self.objects[name]
+  }
+  env := self
+  for env != nil {
+    if alt := env.objects[name]; alt != nil {
+      return alt
+    }
+    env = self.outer
+  }
+  return nil
+}
